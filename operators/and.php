a2be51075d11fb493f01a78475f000409275922e
tebbaa-adnane <razorzeroday@gmail.com>
2022-01-25 15:41:07 +0100

    🚀

---
+++ operators/and.php
@@ -0,0 +1,12 @@
+<?php 
+
+// and or &&
+
+$price = 100;
+$qty = 5;
+$discounted = $qty > 3 && $price > 99;
+// var_dump
+var_dump($discounted);
+
+
+?>
---
+++ operators/comparison-operators.php
@@ -0,0 +1,12 @@
+<?php 
+
+// Simple comparison 
+$x = 10; 
+$y = 10;
+var_dump($x == $y); 
+//
+$a = 10;
+$b = 20;
+var_dump($b != $a); 
+
+?>
