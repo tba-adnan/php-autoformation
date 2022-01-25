a4bfcb211cce8241101c96c1501dd12a0e9d412d
tebbaa-adnane <razorzeroday@gmail.com>
2022-01-25 12:43:59 +0100

    .

---
+++ operators.php
@@ -0,0 +1,14 @@
+<?php 
+$data1 = $data2 = "Tebbaa adnane ";
+echo $data1; 
+//
+$number1 = 14;
+$number1 += 1;
+echo $number1;
+//
+$name = " adnane ";
+$info = " is a 18 years old Solicode student ";
+$data = $name. $info; 
+echo $data; 
+//
+?>
