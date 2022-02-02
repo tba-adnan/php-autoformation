ac43529a0f94b927c5a1ac205d779454b5144f4e
tebbaa-adnane <razorzeroday@gmail.com>
2022-02-02 16:07:20 +0100

    add/fix

---
+++ OOP/example1.php
@@ -0,0 +1,16 @@
+<?php
+
+// Global php OOP seen
+
+class BankAccount // Defines a class
+{
+    public $accountNumber; // Property 1
+    public $balance; // Property 2
+}
+
+$account = new BankAccount(); 
+//
+$account->accountNumber = 1; // setting Property1
+$account->balance = 100; // setting Property 2
+
+?>
---
+++ sessions/sessions.php
@@ -0,0 +1,12 @@
+<?php 
+
+// cookie != session.
+
+// Start the session
+session_start();
+// Set session variables
+$_SESSION["favcolor"] = "green";
+$_SESSION["favanimal"] = "cat";
+echo "Session variables are set.";
+
+?>
